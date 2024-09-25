ba58792a81726442e9510b0a037c495d83734221
jofin15 <70406065+jofin15@users.noreply.github.com>
2024-09-25 07:26:28 +0000

    first commit

--- script.py
+++ script.py
@@ -1,13 +1,1 @@
-import os
-from flask import Flask
-
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def mainpage():
-	return "Hello from my python server"
-
-if(__name__=='__main__'):
-	app.run(host='0.0.0.0',port=5000,debug=True)
+print("Wassup Buddy!!")
